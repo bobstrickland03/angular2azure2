0edeb3583a33cd1cf845abc881bb8160a2680f8c
Robert Strickland <robert.strickland@pearson.com>
2016-08-10 15:24:39 -0600

    blah

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Monkey Bob has it working :-)</h1>'
+    template: '<h1>Wow I am a geek :)</h1>'
 })
 export class AppComponent { }
