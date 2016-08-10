24191fda02d3876dc2be6ddd14f405cc30f2edb8
Robert Strickland <robert.strickland@pearson.com>
2016-08-10 15:35:05 -0600

    another change for testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Wow I am a geek :)</h1>'
+    template: '<h1>Has lots of potential</h1>'
 })
 export class AppComponent { }
